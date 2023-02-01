06fc86320756987b056652e6f3b90a75cf583597
hugobbi <hgugobbi@gmail.com>
2023-02-01 17:04:56 -0300

    Adicionando arquivo py e fontes boas

---
+++ needleman_wunsch.py
@@ -0,0 +1,9 @@
+import numpy
+
+# https://studentsxstudents.com/coding-global-sequence-alignment-using-the-needleman-wunsch-algorithm-d47971ebbe5
+# https://pt.wikipedia.org/wiki/Algoritmo_Needleman-Wunsch
+# https://gist.github.com/slowkow/06c6dba9180d013dfd82bec217d22eb5
+
+def needleman_wunsch():
+    return 1
+
