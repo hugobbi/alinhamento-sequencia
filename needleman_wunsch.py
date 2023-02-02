2fdbaf1d6458b4dc49df11233feff1de24a2a7e8
hugobbi <hgugobbi@gmail.com>
2023-02-02 13:20:59 -0300

    Adicionando backtracking

--- needleman_wunsch.py
+++ needleman_wunsch.py
@@ -15,8 +15,8 @@ def needleman_wunsch(seq1, seq2, match, mismatch, gap):
     n_seq2 = len(seq2)
 
     M = np.zeros((n_seq1, n_seq2))
-    M[:,0] = np.linspace(0, -gap*(n_seq1-1), n_seq1)
-    M[0,:] = np.linspace(0, -gap*(n_seq2-1), n_seq2)
+    M[:,0] = np.linspace(0, gap*(n_seq1-1), n_seq1)
+    M[0,:] = np.linspace(0, gap*(n_seq2-1), n_seq2)
 
     possiveis_scores = np.zeros(3)
     for i in range(1, n_seq1):
@@ -28,7 +28,45 @@ def needleman_wunsch(seq1, seq2, match, mismatch, gap):
 
     print(M)
 
+    alinhamento_1 = ""
+    alinhamento_2 = ""
+    i = n_seq1-1
+    j = n_seq2-1
+    while i > 0 and j > 0:
+        score = M[i,j]
+        score_diag = M[i-1, j-1]
+        score_cima = M[i, j-1]
+        score_esq = M[i-1, j]
+
+        if score == score_diag + calcula_score(seq1[i-1], seq2[j-1], match, mismatch):
+            alinhamento_1 += seq1[i-1]
+            alinhamento_2 += seq2[j-1]
+            i -= 1
+            j -= 1
+        elif score == score_esq + gap:
+            alinhamento_1 += seq1[i-1]
+            alinhamento_2 += '-'
+            i -= 1
+        elif score == score_cima + gap:
+            alinhamento_1 += '-'
+            alinhamento_2 += seq2[j-1]
+            j -= 1 
     
+    while i > 0:
+        alinhamento_1 += seq1[i-1]
+        alinhamento_2 += '-'
+        i -= 1
+
+    while j > 0:
+        alinhamento_1 += '-'
+        alinhamento_2 += seq2[j-1]
+        j -= 1
+    
+    #alinhamento_1_final = alinhamento_1[::-1]
+    #alinhamento_2_final = alinhamento_2[::-1]
+
+    print(alinhamento_1)
+    print(alinhamento_2)
 
 needleman_wunsch("ATCG", "CTCG", 2, -2, -3)
 
